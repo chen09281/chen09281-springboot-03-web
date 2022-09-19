89d8bd90462abae6d6b5f25a554d0e2d85402aec
Chen09281 <weifengchen198@gmail.com>
2022-09-19 22:51:11 +0800

    🚀 添加添加员工功能

--- src/main/java/com/chen/controller/EmployeeController.java
+++ src/main/java/com/chen/controller/EmployeeController.java
@@ -1,10 +1,14 @@
 package com.chen.controller;
 
+import com.chen.dao.DepartmentDao;
 import com.chen.dao.EmployeeDao;
+import com.chen.pojo.Department;
 import com.chen.pojo.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Collection;
@@ -14,6 +18,8 @@ public class EmployeeController {
 
     @Autowired
     EmployeeDao employeeDao;
+    @Autowired
+    DepartmentDao departmentDao;
 
     // 查询所有的员工
     @RequestMapping("/emps")
@@ -21,5 +27,20 @@ public class EmployeeController {
         Collection<Employee> employees = employeeDao.getAll();
         model.addAttribute("emps",employees);
         return "emp/list";
+    }
+    @GetMapping("/emp")
+    public String toAddPage(Model model){
+        // 查出所有部门的信息
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("departments",departments);
+        return "emp/add";
+    }
+
+    @PostMapping("/emp")
+    public String addEmp(Employee employee){
+        // 添加的操作 forward
+        System.out.println("save=>"+employee);
+        employeeDao.save(employee); // 调用底层业务方法保存员工的信息
+        return "redirect:/emps";
     }
 }
